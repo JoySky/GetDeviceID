cba5fe183c35e4449a9b6e40bdc06eefb365e43a
JoySky <joysky@outlook.com>
2015-05-19 16:06:51 +0800

    WINCE获得PDA的机器码
    获得PDA的机器码，然后通过计算该机器码，获得程序的注册码

---
+++ GetDeviceID/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace GetDeviceID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "确定";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(234, 262);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.Text = "机器码：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "关闭";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(28, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 60);
+            this.label2.Text = "提示：运行程序后，点击确定，将获得该机器的机器码。请认真仔细的记录下该码，并将它发给我们。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.panel1);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "获得机器码";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GetDeviceID/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace GetDeviceID
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("coredll.dll")]
+        private extern static int GetDeviceUniqueID([In, Out] byte[] appdata,
+                                                    int cbApplictionData,
+                                                    int dwDeviceIDVersion,
+                                                    [In, Out] byte[] deviceIDOuput,
+                                                    out uint pcbDeviceIDOutput);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] buffer = GetDeviceID("DARMSYZX");  
+            StringBuilder sb = new StringBuilder();  
+            for (int x = 0; x < buffer.Length; x++)  
+            {  
+                //sb.Append('{');  
+                sb.Append(string.Format("{0:x2}", buffer[x]));  
+                //sb.Append("} ");  
+            }
+            string tempCode = sb.ToString();
+            string strCode = tempCode.Substring(0, 16).ToUpper();
+            string str1 = strCode.Substring(0, 4);
+            string str2 = strCode.Substring(4, 4);
+            string str3 = strCode.Substring(8, 4);
+            string str4 = strCode.Substring(12, 4);
+            textBox1.Text = str1 + "-" + str2 + "-" + str3 + "-" + str4;  
+
+        }
+
+        private byte[] GetDeviceID(string AppString)  
+        {  
+            // Call the GetDeviceUniqueID  
+            byte[] AppData = new byte[AppString.Length];  
+            for (int count = 0; count < AppString.Length; count++)  
+            AppData[count] = (byte)AppString[count];  
+            int appDataSize = AppData.Length;  
+            byte[] DeviceOutput = new byte[20];             
+            uint SizeOut = 20;  
+            GetDeviceUniqueID(AppData, appDataSize, 1, DeviceOutput, out SizeOut);  
+            return DeviceOutput;  
+       }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }  
+
+        
+    }
+}
